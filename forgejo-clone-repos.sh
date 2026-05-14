8a89342909d52f3da7a75d959442d6d594330c62
mihaylenko_sv <mihaylenko_sv@mosvodokanal.ru>
2026-05-14 16:22:26 +0300

    fix feadme

--- forgejo-clone-repos.sh
+++ forgejo-clone-repos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Получить список репозиториев из API и клонировать их
 # Если нет jq то можно скачать и поместить каталог где установлен Bash https://github.com/stedolan/jq/releases/latest/download/jq-win64.exe
-REPOSITORIES=$(curl -s https://git-forgejo.serpan.site/api/v1/users/SergeyMi/repos?per_page=1000 | jq -r '.[] | select(.fork == false).clone_url')
+REPOSITORIES=$(curl -s https://codeberg.org/api/v1/users/SergeyMi/repos?per_page=1000 | jq -r '.[] | select(.fork == false).clone_url')
 for REPOSITORY in $REPOSITORIES; do
   git clone $REPOSITORY
 done
